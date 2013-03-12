7e082ada8451e306f610f97ca2a8c6d2dd1ffaba
Homidov Bakhtiyor <bakhtiyor.h@gmail.com>
2013-03-12 14:59:02 +0500

    Fix indentations in files

--- README.md
+++ README.md
@@ -9,53 +9,53 @@ Examples:
 ### Numeric methods
 
 ```ruby
-"1".is_integer?							#=> true
-"1.0".is_integer?						#=> false
-"not_numeric".is_integer?		#=> false
+"1".is_integer?             #=> true
+"1.0".is_integer?           #=> false
+"not_numeric".is_integer?   #=> false
 ```
 
 ```ruby
-"1".is_numeric?							#=> true
-"1.0".is_numeric?						#=> true
-"not_numeric".is_numeric?		#=> false
+"1".is_numeric?             #=> true
+"1.0".is_numeric?           #=> true
+"not_numeric".is_numeric?   #=> false
 ```
 
 ```ruby
-"1".is_float?							#=> false
-"1.0".is_float?						#=> true
-"not_numeric".is_float?		#=> false
+"1".is_float?             #=> false
+"1.0".is_float?           #=> true
+"not_numeric".is_float?   #=> false
 ```
 
 ```ruby
-"1".to_numeric								#=> 1
-"1".to_numeric.class					#=> Fixnum
-"1.0".to_numeric							#=> 1.0
-"1.0".to_numeric.class				#=> Float
-("1" * 10).to_numeric					#=> 1111111111
-("1" * 10).to_numeric.class		#=> Bignum
-"not_numeric".to_numeric			#=> nil	
+"1".to_numeric                #=> 1
+"1".to_numeric.class          #=> Fixnum
+"1.0".to_numeric              #=> 1.0
+"1.0".to_numeric.class        #=> Float
+("1" * 10).to_numeric         #=> 1111111111
+("1" * 10).to_numeric.class   #=> Bignum
+"not_numeric".to_numeric      #=> nil 
 ```
 
 ### Decode/encode methods
 
 ```ruby
-"hello world!".decode_string(:hex) 	#=> 68656c6c6f20776f726c6421
-"hi".decode_string(:bin)						#=> 0110100001101001
+"hello world!".decode_string(:hex)  #=> 68656c6c6f20776f726c6421
+"hi".decode_string(:bin)            #=> 0110100001101001
 
-"NBSWY3DPEB3W64TMMQQQ====".decode_string(:base32) 	#=> hello world!
-"aGVsbG8gd29ybGQh\n".decode_string(:base64) 				#=> hello world!  
+"NBSWY3DPEB3W64TMMQQQ====".decode_string(:base32)   #=> hello world!
+"aGVsbG8gd29ybGQh\n".decode_string(:base64)         #=> hello world!  
 
-"aGVsbG8gd29ybGQh".decode_string(:base64, strict: true)	#=> hello world!
+"aGVsbG8gd29ybGQh".decode_string(:base64, strict: true) #=> hello world!
 ```
 
 ```ruby
-"68656c6c6f20776f726c6421".decode_string(:hex) 	#=> hello world!
-"0110100001101001".decode_string(:bin)					#=> hi
+"68656c6c6f20776f726c6421".decode_string(:hex)  #=> hello world!
+"0110100001101001".decode_string(:bin)          #=> hi
 
-"hello world!".decode_string(:base32) 	#=> NBSWY3DPEB3W64TMMQQQ====
-"hello world!".decode_string(:base64) 	#=> aGVsbG8gd29ybGQh\n  
+"hello world!".decode_string(:base32)   #=> NBSWY3DPEB3W64TMMQQQ====
+"hello world!".decode_string(:base64)   #=> aGVsbG8gd29ybGQh\n  
 
-"hello world!".decode_string(:base64, strict: true)	#=> aGVsbG8gd29ybGQh	
+"hello world!".decode_string(:base64, strict: true) #=> aGVsbG8gd29ybGQh  
 ```
 
 
--- spec/rcore_ext/string/numeric_spec.rb
+++ spec/rcore_ext/string/numeric_spec.rb
@@ -9,88 +9,88 @@ describe "String numeric" do
   let(:big_num)      { {act: "1" * 10,         exp: 1111111111} }
 
   describe "respond to numeric methods" do
-	  it "should respond to is_integer? method" do
-	    "".should respond_to :is_integer?
-	  end
+    it "should respond to is_integer? method" do
+      "".should respond_to :is_integer?
+    end
 
-	  it "should respond to is_float? method" do
-	    "".should respond_to :is_float?
-	  end
+    it "should respond to is_float? method" do
+      "".should respond_to :is_float?
+    end
 
-	  it "should respond to is_numeric? method" do
-	    "".should respond_to :is_numeric?
-	  end
+    it "should respond to is_numeric? method" do
+      "".should respond_to :is_numeric?
+    end
 
-	  it "should respond to to_numeric method" do
-	    "".should respond_to :to_numeric
-	  end
-	end
+    it "should respond to to_numeric method" do
+      "".should respond_to :to_numeric
+    end
+  end
 
   describe "is_integer? method functionality" do
-	  it "should be an integer value" do
-	    fix_num[:act].is_integer?.should be_true
-	  end
+    it "should be an integer value" do
+      fix_num[:act].is_integer?.should be_true
+    end
 
-	  it "a big number should be an integer value" do
-	    big_num[:act].is_integer?.should be_true
-	  end
+    it "a big number should be an integer value" do
+      big_num[:act].is_integer?.should be_true
+    end
 
-	  it "should not be an integer value" do
-	    not_numeric[:act].is_integer?.should be_false
-	  end
+    it "should not be an integer value" do
+      not_numeric[:act].is_integer?.should be_false
+    end
 
-	  it "a float value should not be an integer value" do
-	    float[:act].is_integer?.should be_false
-	  end
-	end
+    it "a float value should not be an integer value" do
+      float[:act].is_integer?.should be_false
+    end
+  end
 
   describe "is_numeric? method functionality" do
-	  it "should be a numeric value" do
-	    fix_num[:act].is_numeric?.should be_true
-	  end
+    it "should be a numeric value" do
+      fix_num[:act].is_numeric?.should be_true
+    end
 
-	  it "a big number should be a numeric value" do
-	    big_num[:act].is_numeric?.should be_true
-	  end
+    it "a big number should be a numeric value" do
+      big_num[:act].is_numeric?.should be_true
+    end
 
-	  it "should not be a numeric value" do
-	    not_numeric[:act].is_numeric?.should be_false
-	  end
+    it "should not be a numeric value" do
+      not_numeric[:act].is_numeric?.should be_false
+    end
 
-	  it "a float value should be a numeric value" do
-	    float[:act].is_numeric?.should be_true
-	  end
-	end
+    it "a float value should be a numeric value" do
+      float[:act].is_numeric?.should be_true
+    end
+  end
 
   describe "is_float? method functionality" do
-	  it "an integer value should not be a float value" do
-	    fix_num[:act].is_float?.should be_false
-	  end
+    it "an integer value should not be a float value" do
+      fix_num[:act].is_float?.should be_false
+    end
 
-	  it "should not be a float value" do
-	    not_numeric[:act].is_float?.should be_false
-	  end
+    it "should not be a float value" do
+      not_numeric[:act].is_float?.should be_false
+    end
 
-	  it "should be a float value" do
-	    float[:act].is_float?.should be_true
-	  end
-	end
+    it "should be a float value" do
+      float[:act].is_float?.should be_true
+    end
+  end
 
   describe "to_numeric method functionality" do
-	  it "should be converted to an integer value" do
-	    fix_num[:act].to_numeric.should == fix_num[:exp] 
-	  end
+    it "should be converted to an integer value" do
+      fix_num[:act].to_numeric.should == fix_num[:exp] 
+    end
 
-	  it "should be converted to a float value" do
-	    float[:act].to_numeric.should == float[:exp] 
-	  end
+    it "should be converted to a float value" do
+      float[:act].to_numeric.should == float[:exp] 
+    end
 
-	  it "should be converted to a big number value" do
-	    big_num[:act].to_numeric.should == big_num[:exp]
-	  end
+    it "should be converted to a big number value" do
+      big_num[:act].to_numeric.should == big_num[:exp]
+    end
 
-	  it "should not be converted to numeric value" do
-	    not_numeric[:act].to_numeric.should == not_numeric[:exp]
-	  end
-	end
+    it "should not be converted to numeric value" do
+      not_numeric[:act].to_numeric.should == not_numeric[:exp]
+    end
+  end
 end 
